76f2b89ba91bf76c11f398258b835cd95a43d8f6
Robin Landsmann <robin50@web.de>
2017-07-26 10:15:49 +0200

    1. testcommit

---
+++ index.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Summary:
+ *
+ *
+ *
+ *
+ * @author Thomas Teves <teves@systemschmiede.com> Systemschmiede IT-Lösungen 2015
+ *
+ * Copyright (c) 2017, Systemschmiede IT-Lösungen
+ * All rights reserved
+ *
+ * Project:     testgithubrepo
+ * File:        index.php
+ * Created on:  26.07.2017
+ * Programmer:  Thomas Teves
+ *
+ * Redistribution and use in source and binary forms, with or without modification,
+ * are not permitted.
+ *
+ */
+     
